c11664156087fb89045d1746d1f9ed0bcf879e0e
WONG KEAT HONG <Raydivine@users.noreply.github.com>
2014-06-09 16:29:25 +0800

    Create DoubleBubbleSort.h

---
+++ BubbleSort/src/DoubleBubbleSort.h
@@ -0,0 +1,10 @@
+#ifndef DoubleBubbleSort_H
+#define DoubleBubbleSort_H
+
+
+int doubleCompare(void *v1, void *v2);
+void doubleSwap(void *array, int index1, int index2) ;
+
+
+
+#endif // DoubleBubbleSort_H
