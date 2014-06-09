0ad8dd46181f76741169f7f6ca5c95cbf24886ce
WONG KEAT HONG <Raydivine@users.noreply.github.com>
2014-06-09 16:28:06 +0800

    Create DoubleBubbleSort.c

---
+++ BubbleSort/src/DoubleBubbleSort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "DoubleBubbleSort.h"
+
+
+int doubleCompare(void *v1, void *v2) {
+
+	int  *A; 
+	int  *B; 
+	
+	A= (int *)v1;
+	B= (int *)v2;
+	
+     if (*A > *B )
+	 return 1; 
+	 
+     else if( *A ==*B )
+     return 0	;
+	 
+     else 
+     return   -1;
+}
+
+
+void doubleSwap(void *array, int index1, int index2) {
+
+	int temp ;
+	
+	temp=((int *)array)[index2];
+	((int *)array)[index2]=((int *)array)[index1];
+	((int *)array)[index1]=temp;
+}
